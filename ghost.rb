181e1c97080e7a90ee845d61f5a0bad224a393f3
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-06-20 16:40:15 -0700

    added functionality for multiple rounds

--- ghost.rb
+++ ghost.rb
@@ -3,7 +3,7 @@ require 'set'
 class Game
 
   ALPHABET = Set.new(("a".."z").to_a)
-
+  MAX = 2
   attr_accessor :current_player, :dictionary
 
   def initialize(*players)
@@ -12,6 +12,8 @@ class Game
     @fragment = ""
     @players = players
     @current_player = players.first
+    @losses = {}
+    @players.each {|player| @losses[player] = 0}
   end
 
   def over?
@@ -26,33 +28,58 @@ class Game
     @current_player
   end
 
+  def previous_player
+    previous_player = current_player == @players.first ? @players.last : @players.first
+  end
+
   def next_player
     @current_player = current_player == @players.first ? @players.last : @players.first
   end
 
   def play_turn
     letter = nil
-    until letter
+    until letter && valid_play?(letter)
       letter = @current_player.guess
-      if (valid_play?(letter))
-        next
-      end
     end
     change_fragment(letter)
   end
 
   def play_round
-
+    @fragment = ""
     until over?
       play_turn
       next_player
     end
-    puts "Game over"
+    update_standings
+  end
+
+  def run
+    @fragment = ""
+    until game_over?
+      play_round
+    end
+    puts "Game is over"
+  end
+
+  def game_over?
+    @players.length == 1
+  end
+
+  def update_standings
+    puts "#{previous_player.name} spelled #{@fragment}"
+    puts "#{previous_player.name} gains a point"
+    @losses[previous_player] += 1
+    if @losses[previous_player] === MAX
+      puts "Game is Over"
+      puts "#{previous_player.name} has been eliminated"
+      @players.delete(previous_player)
+    end
+    sleep(2)
   end
 
   def valid_play?(letter)
     return true if ALPHABET.include?(letter)
-    true
+    false
   end
 
 end
@@ -81,4 +108,4 @@ p1 = Player.new("first")
 p2 = Player.new("second")
 game = Game.new(p1,p2)
 
-game.play_round
+game.run
